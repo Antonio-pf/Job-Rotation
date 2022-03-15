00c35a998b2560a2371135f5d48c9db071651daf
Antonio-pf <antoniopfelipe03@gmail.com>
2022-03-15 14:32:30 -0300

    Interte String

---
+++ src/Exer05.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Exer05 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String palavra = "";
+        String palavraInversa = "";
+        System.out.println("Digite uma palavra: ");
+        palavra = scan.next();
+
+        for(int i = palavra.length() - 1; i >= 0; i--){//começa do final para o começo
+            palavraInversa = palavraInversa + palavra.charAt(i);
+
+        }
+        System.out.println(palavraInversa);
+    }
+}
