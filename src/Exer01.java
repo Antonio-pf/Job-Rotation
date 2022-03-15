12af7d210b58a2dcea97a6f12728c4c5a5e8625b
Antonio-pf <antoniopfelipe03@gmail.com>
2022-03-15 13:42:24 -0300

    For

---
+++ src/Exer01.java
@@ -0,0 +1,13 @@
+public class Exer01 {
+    public static void main(String[] args) {
+
+        int soma = 0, indice = 13;
+        // k = i
+        for(int i = 0; i < 13; i ++  ){
+            i++;
+            soma = soma + i;
+
+        }
+        System.out.println(soma);
+    }
+}
